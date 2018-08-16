07c4a2bbd736e95d93d59be25a7901b7b42b683d
Jānis Ošenieks <janis.osenieks@cube.lv>
2018-08-16 10:39:14 +0300

    add model docs

--- src/Models/ArboryMenu.php
+++ src/Models/ArboryMenu.php
@@ -2,9 +2,24 @@
 
 namespace CubeAgency\ArboryMenu\Models;
 
+use Barryvdh\LaravelIdeHelper\Eloquent;
 use Illuminate\Database\Eloquent\Model;
 
-
+/**
+ * Class ArboryMenu
+ * @package CubeAgency\ArboryMenu\Models
+ *
+ * @property int                                                            $id
+ * @property \Carbon\Carbon|null                                            $created_at
+ * @property \Carbon\Carbon|null                                            $updated_at
+ * @property string                                                         $name
+ * @property-read \Illuminate\Database\Eloquent\Collection|ArboryMenuItem[] $items
+ * @method static \Illuminate\Database\Eloquent\Builder|ArboryMenu whereCreatedAt( $value )
+ * @method static \Illuminate\Database\Eloquent\Builder|ArboryMenu whereId( $value )
+ * @method static \Illuminate\Database\Eloquent\Builder|ArboryMenu whereName( $value )
+ * @method static \Illuminate\Database\Eloquent\Builder|ArboryMenu whereUpdatedAt( $value )
+ * @mixin Eloquent
+ */
 class ArboryMenu extends Model
 {
     /**
--- src/Models/ArboryMenuItem.php
+++ src/Models/ArboryMenuItem.php
@@ -3,8 +3,31 @@
 namespace CubeAgency\ArboryMenu\Models;
 
 use Arbory\Base\Content\Relation;
+use Barryvdh\LaravelIdeHelper\Eloquent;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class ArboryMenuItem
+ * @package CubeAgency\ArboryMenu\Models
+ *
+ * @property int                                               $id
+ * @property \Carbon\Carbon|null                               $created_at
+ * @property \Carbon\Carbon|null                               $updated_at
+ * @property int                                               $menu_id
+ * @property int|null                                          $position
+ * @property string|null                                       $name
+ * @property string|null                                       $link
+ * @property-read \Illuminate\Database\Eloquent\Model|Eloquent $owner
+ * @property-read \Arbory\Base\Content\Relation                $relation
+ * @method static \Illuminate\Database\Eloquent\Builder|ArboryMenuItem whereCreatedAt( $value )
+ * @method static \Illuminate\Database\Eloquent\Builder|ArboryMenuItem whereId( $value )
+ * @method static \Illuminate\Database\Eloquent\Builder|ArboryMenuItem whereMenuId( $value )
+ * @method static \Illuminate\Database\Eloquent\Builder|ArboryMenuItem wherePosition( $value )
+ * @method static \Illuminate\Database\Eloquent\Builder|ArboryMenuItem whereUpdatedAt( $value )
+ * @method static \Illuminate\Database\Eloquent\Builder|ArboryMenuItem whereLink( $value )
+ * @method static \Illuminate\Database\Eloquent\Builder|ArboryMenuItem whereName( $value )
+ * @mixin Eloquent
+ */
 class ArboryMenuItem extends Model
 {
     /**
